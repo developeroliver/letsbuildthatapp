d60a0e5682806f6e4696a0baa697f6abc3286684
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-28 09:45:31 +0200

    remove storyboard

---
+++ Compagnies/Compagnies/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Compagnies
+//
+//  Created by olivier geiger on 28/04/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Compagnies/Compagnies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compagnies
+//
+//  Created by olivier geiger on 28/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
